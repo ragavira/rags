7113d3e7f87f08948fc08af0535c884b771b6cdb
ragavira <47376338+ragavira@users.noreply.github.com>
2020-02-07 11:45:45 +0530

    Create nk.py

---
+++ nk.py
@@ -0,0 +1,10 @@
+n,k=map(int,input().split(","))
+count=-1
+v=list(map(int,input().split(",")))[:n]
+for i in range(n) :
+  if v[i]==k :
+    count+=1
+if count>-1 :
+   print(count)
+else :
+   print(-1)
